feac73ef6f15094a767ec6025c16ec6dfbaaa994
kylle <kylle@naver.com>
2019-12-27 23:14:39 +0900

    알고리즘 공부

--- 1.수열/src/수열/E분수더하기빼기.java
+++ 1.수열/src/수열/E분수더하기빼기.java
@@ -31,14 +31,14 @@ public class E분수더하기빼기 {
 		startTime = System.currentTimeMillis(); 
 		for(int repeat=0 ; repeat<1000000 ; repeat++) {		
 		
-			  int i = 0 ;  double sum = 1; int sw = 0;
-			  while (i<100) {
+			  float i = 0 ;  float sum = 0; int sw = 0;
+			  while (i<99) {
 				  i++;
 				  if(sw==0) {
-					  sum *= -i;
+					  sum -= i/(i+1);
 					  sw = 1;
 				  }else {
-					  sum *= i;
+					  sum += i/(i+1);
 					  sw = 0;
 				  }
 			  }
@@ -55,12 +55,13 @@ public class E분수더하기빼기 {
 		startTime = System.currentTimeMillis(); 
 		for(int repeat=0 ; repeat<1000000 ; repeat++) {		
 		
-			  int i = 0 ;  double sum = 1;
-			  while (i<100) {
+			  float i = 0 ;  float sum = 0;
+			  while (true) {
 				  i++;
-				  sum *= -i;
+				  sum -= i/(i+1);
+				  if(i>=99) break;
 				  i++;
-				  sum *= i;				  
+				  sum += i/(i+1);				  
 			  }
 			  if(repeat==1) System.out.println(sum);
 		
