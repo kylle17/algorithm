7068492430655b4934c131b3626b376dc2d78390
kylle <kylle@naver.com>
2020-01-31 19:58:39 +0900

    알고리즘 -  배열(기본 5행 5열)

---
+++ 알고리즘JAVA/src/DE_배열/A_기본_5행_5열.java
@@ -0,0 +1,23 @@
+package DE_배열;
+
+public class A_기본_5행_5열 {
+	public static void main(String[] args) {
+		int i, j, k;
+		int a[][] = new int[5][5];
+
+		k = 0;
+		for (i = 0; i <= 4; i++) {
+			for (j = 0; j <= 4; j++) {
+				k++;
+				a[i][j] = k;
+			}
+		}
+
+		for (int x = 0; x <= 4; x++) {
+			for (int y = 0; y <= 4; y++) {
+				System.out.print(" "+a[x][y]);
+			}
+			System.out.println();
+		}
+	}
+}
