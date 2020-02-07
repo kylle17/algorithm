b0bc2f2c47068a02adbf0b4099ef7b57297d48a2
kylle <kylle@naver.com>
2020-02-07 20:55:18 +0900

    알고리즘 - 모래시계 만들기

---
+++ 알고리즘JAVA/알고리즘/src/DE_배열/H_모래시계_만들기.java
@@ -0,0 +1,36 @@
+package DE_배열;
+
+import java.util.Scanner;
+
+public class H_모래시계_만들기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		x = sc.nextInt();
+		int a[][] = new int[x][x];
+		int n = 0, s = 0, e = x , m=x/2;
+
+		for (int i = 0; i < x; i++) {
+			for (int j = s; j < e; j++) {
+				n++;
+				a[i][j] = n;
+			}
+			if (i < m) {
+				s++;
+				e--;
+			}
+			else {
+				s--;
+				e++;
+			}
+		}
+
+		for (int i = 0; i <x ; i++) {
+			for (int j = 0; j <x ; j++) {
+				System.out.print(" "+a[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
